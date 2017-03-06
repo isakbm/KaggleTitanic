dfe9c2cab927adf112b564126966e97ecd26e813
Isak Buhl-Mortensen <buhlmort@nbi.ku.dk>
2017-03-06 11:39:12 +0100

    First commit

---
+++ Program.fs
@@ -0,0 +1,27 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+open FSharp.Data
+
+
+
+type Stocks = CsvProvider<"../data/MSFT.csv", ",">
+
+[<EntryPoint>]
+let main argv = 
+    
+    let msft = Stocks.Load("http://ichart.finance.yahoo.com/table.csv?s=MSFT")
+
+ 
+
+    msft |> printfn "%A"
+ 
+    for row in msft.Rows do
+        row |> printfn "%A"
+
+
+    
+    
+    
+    0 // return an integer exit code
+    
